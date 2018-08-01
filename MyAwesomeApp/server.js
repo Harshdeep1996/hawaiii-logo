a961d93cf1b5593985c11e00f9629a7ab1c5a431
Harshdeep <harsh.deep@student.manchester.ac.uk>
2018-08-01 10:36:07 +0530

    checking if the bug is solved by serving using express

---
+++ MyAwesomeApp/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(`${__dirname}/ui-react/build`));
+
+app.get('/api/hello', function (req, res){
+	res.json({message: "Hello World"});
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, function() {
+	console.log("Server listening on port " + port);
+})
